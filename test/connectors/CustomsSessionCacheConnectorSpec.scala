19eb7601805a3f2ad9ae1c691de4d11963157465
harsha <29434052+harshare@users.noreply.github.com>
2023-05-09 19:29:20 +0100

    MMCA-3983 Append northern ireland text to XI EORI DD accounts

--- test/connectors/CustomsSessionCacheConnectorSpec.scala
+++ test/connectors/CustomsSessionCacheConnectorSpec.scala
@@ -40,6 +40,17 @@ class CustomsSessionCacheConnectorSpec extends SpecBase {
       }
     }
 
+    "return account link for sessionId and linkId" in new Setup {
+      val customsSessionCacheUrl = "http://localhost:9840/customs/session-cache/account-link/12345/Ab123"
+
+      when[Future[AccountLink]](mockHttpClient.GET(ArgumentMatchers.eq(customsSessionCacheUrl), any, any)(any, any, any))
+        .thenReturn(Future.successful(AccountLink("GB123","1234567", "a67dhdfkd8sf","Open", Some(0), true)))
+      running(app) {
+        val result = await(customsSessionCacheConnector.getAccountLink("12345", "Ab123")(hc))
+        result mustBe Some(AccountLink("GB123","1234567", "a67dhdfkd8sf","Open", Some(0), true))
+      }
+    }
+
   "return false when failed to submit the request" in new Setup {
       val customsSessionCacheUrl = "http://localhost:9840/customs/session-cache/account-link/12345/Ab123"
 
