d94866edb074df0adfb5fc86ede6509a46e46120
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-05-22 10:56:38 +0100

    MMCA-4009 : Update version & configuration for CDS Financials service - April - V4 (#81)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.1.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.3.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.2.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.7.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "tax-year" % "3.1.0"
+    "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
 
   val test = Seq(
