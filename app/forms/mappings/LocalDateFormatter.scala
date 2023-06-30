2edc3d291029264c014bc3388cc28bb48f233ca1
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2023-06-30 13:54:44 +0100

    Fxing the Date error identification

--- app/forms/mappings/LocalDateFormatter.scala
+++ app/forms/mappings/LocalDateFormatter.scala
@@ -38,13 +38,11 @@ private[mappings] class LocalDateFormatter(
   private def toDate(key: String, month: Int, year: Int): Either[Seq[FormError], LocalDate] =
   validMonth(month) match {
       case true =>
-        Try(LocalDate.of(year, month, 1)) match {
-          case Success(date) =>
-            Right(date)
-          case Failure(_) =>
-            Left(Seq(FormError(key, invalidYear, args)))
+        Try(LocalDate.of(year, month,1)) match {
+          case Success(date) => Right(date)
+          case Failure(_) => Left(Seq(FormError(updateFormErrorKeys(key, month, year), invalidYear, args)))
         }
-      case _ => Left(Seq(FormError(key, invalidMonth, args)))
+      case _ => Left(Seq(FormError(updateFormErrorKeys(key, month, year), invalidMonth, args)))
     }
 
   private def formatDate(key: String, data: Map[String, String]): Either[Seq[FormError], LocalDate] = {
@@ -62,11 +60,12 @@ private[mappings] class LocalDateFormatter(
       nonNumericKey = invalidYear,
       args
     )
+    
 
     for {
-      month <- intMonth.bind(s"$key.month", data).right
-      year  <- intYear.bind(s"$key.year", data).right
-      date  <- toDate(key, month, year).right
+      month <- intMonth.bind(s"$key.month", data)
+      year  <- intYear.bind(s"$key.year", data)
+      date  <- toDate(key, month, year)
     } yield date
   }
 
@@ -89,15 +88,15 @@ private[mappings] class LocalDateFormatter(
         }
       case 1 =>
         (key, missingFields.head) match {
-        case ("start", "month") => Left(List(FormError(key, emptyStartMonth, args)))
-        case ("start", "year") => Left(List(FormError(key, emptyStartYear, args)))
-        case ("end", "month") => Left(List(FormError(key, emptyEndMonth, args)))
-        case ("end", "year") => Left(List(FormError(key, emptyEndYear, args)))
+        case ("start", "month") => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyStartMonth, args)))
+        case ("start", "year") => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyStartYear, args)))
+        case ("end", "month") => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyEndMonth, args)))
+        case ("end", "year") => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyEndYear, args)))
       }
       case _ =>
         (key, missingFields) match {
-          case ("start", List("month", "year")) => Left(List(FormError(key, emptyStartDate, args)))
-          case ("end", List("month","year")) => Left(List(FormError(key, emptyEndDate, args)))
+          case ("start", List("month", "year")) => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyStartDate, args)))
+          case ("end", List("month","year")) => Left(List(FormError(formErrorKeysInCaseOfEmptyOrNonNumericValues(key, data), emptyEndDate, args)))
         }
       }
   }
@@ -107,6 +106,52 @@ private[mappings] class LocalDateFormatter(
       s"$key.month" -> value.getMonthValue.toString,
       s"$key.year" -> value.getYear.toString
     )
+
+    /**
+   * Updates the FormError key as per valid day, month and year criteria
+   * key is updated as below
+   * invalid day - key.day
+   * invalid month - key.month
+   * invalid year - key.year
+   *
+   * @param key Input form key
+   * @param day Day value of the input date
+   * @param month Month value of the input date
+   * @param year Year value of the input date
+   * @return Updated form key
+   */
+  private[mappings] def updateFormErrorKeys(key: String,
+                                            month: Int,
+                                            year: Int): String =
+    (month, year) match {
+      case (m, _) if m < 1 || m > 12 => s"$key.month"
+      case (_, y) if y < 1000 || y > 99999 => s"$key.year"
+      case _ => s"$key.day"
+    }
+
+  /**
+   * Updates the FormError key as per the given criteria
+   * key is updated as below
+   * empty day or non numeric day - key.day
+   * empty month or non numeric month - key.month
+   * empty year or non numeric year - key.year
+   *
+   * @param key FormError key
+   * @param data Map of Form values
+   * @return Updated FormError key
+   */
+  private[mappings] def formErrorKeysInCaseOfEmptyOrNonNumericValues(key: String,
+                                                                     data: Map[String, String]): String = {
+
+    val monthValue = data.get(s"$key.month")
+    val yearValue = data.get(s"$key.year")
+
+    (monthValue, yearValue) match {
+      case (Some(m), _) if m.trim.isEmpty || Try(m.trim.toInt).isFailure => s"$key.month"
+      case (_, Some(y)) if y.trim.isEmpty || Try(y.trim.toInt).isFailure => s"$key.year"
+      case _ => s"$key.day"
+    }
+  }
 
   private def validMonth(month: Int): Boolean = {
     month > 0 && month < 13
---
+++ test/helpers/FormHelperSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helpers
+
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import base.SpecBase
+
+class FormHelperSpec extends SpecBase {
+  "updateFormErrorKeyForTheMessage" must {
+    "append .day in the FormError key when key is either start or end and error msg key is " +
+      "among future date, ETMP or Tax year" in new SetUp {
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.historic.document.request.form.error.start.month.date-number-invalid") shouldBe s"$startKey.day"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.historic.document.request.form.error.start.date-missing") shouldBe s"$startKey.day"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.historic.document.request.form.error.start.month.invalid") shouldBe s"$startKey.day"
+    }
+
+    "append .year in the FormError key when key is either start or end and " +
+      "error msg key is of invalid year length" in new SetUp {
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.historic.document.request.form.error.year.invalid-length") shouldBe s"$startKey.year"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.historic.document.request.form.error.year.invalid-length") shouldBe s"$endKey.year"
+    }
+
+    "return the unchanged key when key in neither start or end" in new SetUp {
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        defaultKey, "cf.historic.document.request.form.error.year.invalid-length") shouldBe defaultKey
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        defaultKey, "cf.historic.document.request.form.error.year.invalid-length") shouldBe defaultKey
+    }
+  }
+}
+
+trait SetUp {
+  val startKey = "start"
+  val endKey = "end"
+  val defaultKey = "default"
+}
