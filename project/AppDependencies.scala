0be75d04150b561ead642175b6d2f228ff5adf6d
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-10-17 13:52:53 +0100

    MMCA-4295 : Update version, configuration & warnings for CDS Financia… (#95)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.23.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc" %% "tax-year" % "3.3.0"
@@ -22,8 +22,7 @@ object AppDependencies {
     "com.typesafe.play" %% "play-test" % current,
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.8",
     "org.scalacheck" %% "scalacheck" % "1.17.0",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28"
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
