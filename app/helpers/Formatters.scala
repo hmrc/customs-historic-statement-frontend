3fec408cbaf099991d0849ba7066bf15a8baffc2
Adam Cooper <adam.cooper2@digital.hmrc.gov.uk>
2024-05-29 14:42:19 +0100

    MMCA-4700-Update the formatter tests for the file size threshold calulcators

--- app/helpers/Formatters.scala
+++ app/helpers/Formatters.scala
@@ -40,7 +40,7 @@ trait DateFormatters {
 trait FileFormatters {
 
   private val kbThreshold = 1024
-  private val mbThreshold = 1024 * 1024
+  private val mbThreshold: Int = 1024 * 1024
 
   def fileSize(size: Long): String = size match {
     case kb if kb >= kbThreshold && kb < mbThreshold => s"${kb / kbThreshold}KB"
