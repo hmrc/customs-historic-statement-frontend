90ac5afc76983b93d452adbb3fa9e42603ddda56
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-10-31 19:41:38 +0000

    MMCA-4829 | Refactor PostponedVatViewModel and ImportPostponedVatRequestedStatements (#148)
    * MMCA-4829 | Format
    
    * MMCA-4829 | Remove redundant component
    
    * MMCA-4829 | Update unit tests & update view model
    
    * MMCA-4829 | Update view
    
    * MMCA-4829 | Inline statementDisplayData

--- test/views/ImportPostponedVatRequestedStatementsSpec.scala
+++ test/views/ImportPostponedVatRequestedStatementsSpec.scala
@@ -21,10 +21,16 @@ import models.{EoriHistory, PostponedVATStatement, PostponedVatStatementFile, Po
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.scalatest.Assertion
-import play.twirl.api.Html
-import viewmodels.{PostponedVatStatementsByMonth, PostponedVatStatementsForEori, PostponedVatViewModel, SourceDisplay, StatementDisplayData}
+import play.api.Application
+import play.api.i18n.Messages
+import viewmodels.{
+  PostponedVatStatementsByMonth,
+  PostponedVatStatementsForEori,
+  PostponedVatViewModel,
+  SourceDisplay,
+  StatementDisplayData
+}
 import views.html.ImportPostponedVatRequestedStatements
-
 import java.time.LocalDate
 
 class ImportPostponedVatRequestedStatementsSpec extends ViewTestHelper {
@@ -39,32 +45,25 @@ class ImportPostponedVatRequestedStatementsSpec extends ViewTestHelper {
   }
 
   "PostponedVatViewModel" should {
-    "correctly handle historiesWithDisplayData" in new Setup {
-      val result: Seq[StatementDisplayData] = PostponedVatViewModel.historiesWithDisplayData(postponedVatViewModel.statementsForAllEoris)
-      displayStatementsShouldBeCorrect(result)
+    "correctly handle statementsDisplayData" in new Setup {
+      displayStatementsShouldBeCorrect(viewModel.statementDisplayData)
     }
 
-    "correctly handle groupedStatementsBySource" in new Setup {
-      val result: Map[String, Seq[PostponedVatStatementFile]] = PostponedVatViewModel.groupedStatementsBySource(postponedVatStatementsByMonth_2)
-      result.size mustBe 1
-      result("CDS").length mustBe 1
-      result("CDS").head mustBe postponedVatStatementFile_2
+    "correctly group statements by source" in new Setup {
+      val cdsSource: SourceDisplay = statementData.sources.find(_.source == "CDS").get
+
+      statementData.sources.length mustBe 2
+      cdsSource.files.length mustBe 1
+      cdsSource.files.head mustBe postponedVatStatementFile_2
     }
 
-    "correctly handle renderSourceDisplay" in new Setup {
-      val sourceDisplay: SourceDisplay = SourceDisplay("CDS", Seq(postponedVatStatementFile))
-      val result: Html = PostponedVatViewModel.renderSourceDisplay(sourceDisplay, 0, 0, "October 2020")
-      result.body must include(postponedVatStatementFile.downloadURL)
+    "correctly display statement content" in new Setup {
+      statementData.statementItem.body must include(monthAndYear)
+      statementData.statementItem.body must include(postponedVatStatementFile.downloadURL)
     }
 
-    "correctly handle missingFileMessage for CDS" in new Setup {
-      val result: String = PostponedVatViewModel.missingFileMessage("CDS")
-      result mustBe "No CDS statements available."
-    }
-
-    "correctly handle missingFileMessage for CHIEF" in new Setup {
-      val result: String = PostponedVatViewModel.missingFileMessage("CHIEF")
-      result mustBe "No CHIEF statements available."
+    "correctly handle missing files" in new Setup {
+      statementData.statementItem.body must include(msg("cf.account.postponed-vat.missing-file-type", "CHIEF"))
     }
   }
 
@@ -92,11 +91,15 @@ class ImportPostponedVatRequestedStatementsSpec extends ViewTestHelper {
 
   trait Setup {
 
+    val app: Application = applicationBuilder().build()
+    implicit val msg: Messages = messages(app)
+
     private val someEori = "12345678"
     private val localDateYear = 2020
     private val localDateMonth = 10
     private val localDateMonth2 = 10
     private val localDateDay = 1
+    protected val monthAndYear = "October 2020"
 
     private val filename: String = "name_04"
     private val downloadURL: String = "download_url_06"
@@ -150,5 +153,9 @@ class ImportPostponedVatRequestedStatementsSpec extends ViewTestHelper {
     implicit val view: Document = Jsoup.parse(
       app.injector.instanceOf[ImportPostponedVatRequestedStatements].apply(
         postponedVatViewModel, config.returnLink("postponedVATStatement")).body)
+
+    protected val viewModel: PostponedVatViewModel = PostponedVatViewModel(Seq(postponedVatStatementsForEori))
+    protected val statementDisplayData: Seq[StatementDisplayData] = viewModel.statementDisplayData
+    protected val statementData: StatementDisplayData = statementDisplayData.head
   }
 }
