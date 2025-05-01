c40088c13f8a36ebc7f319cd1cbc6eeb16bf005c
James Crook <48724197+JoniBach@users.noreply.github.com>
2025-05-01 14:38:38 +0100

    MMCA-5295: Fix typo in test

--- test/config/FrontendAppConfigSpec.scala
+++ test/config/FrontendAppConfigSpec.scala
@@ -29,7 +29,7 @@ class FrontendAppConfigSpec extends SpecBase {
         "https://www.gov.uk/guidance/use-a-cash-account-for-cds-declarations"
     }
 
-    "contain the correct GOV survay banner URL" in {
+    "contain the correct GOV survey banner URL" in {
       appConfig.helpMakeGovUkBetterUrl mustBe
         "https://survey.take-part-in-research.service.gov.uk/jfe/form/SV_74GjifgnGv6GsMC?Source=BannerList_HMRC_CDS_MIDVA"
     }
