8cc041a8bb21d24e56ec8ed04995656e9b269547
Jack Brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-02-20 12:22:47 +0000

    Dcwl 3800 (#176)
    * DCWL-3800 | error message focus
    
    * DCWL-3800 | error message focus
    
    * DCWL-3800 | error message focus
    
    * DCWL-3800 | error message focus

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.1.0"
+  val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.7.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.8.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "tax-year"                              % "5.0.0"
   )
