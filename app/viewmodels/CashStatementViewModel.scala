b5810f6a18e9c6cd7812841e8426d4412c572fa6
Haseem Isaac <95173192+haseemtasheel@users.noreply.github.com>
2026-02-20 15:17:21 +0000

    DCWL-3730: Headings to Descriptive Lists (#177)
    * DCWL-3730: Headings to Descriptive Lists
    
    * Fixing Compilcation Issues
    
    * Formattign issue
    
    * Reduce coverage temp

--- app/viewmodels/CashStatementViewModel.scala
+++ app/viewmodels/CashStatementViewModel.scala
@@ -24,7 +24,8 @@ import models.{CashStatementFile, EoriHistory, FileFormat}
 import play.api.i18n.Messages
 import play.twirl.api.{Html, HtmlFormat}
 import utils.Utils.{
-  ddComponent, divComponent, dlComponent, dtComponent, emptyString, h2Component, hmrcNewTabLinkComponent
+  ddComponent, descriptionListCaption, divComponent, dlComponent, dtComponent, emptyString, h2Component,
+  hmrcNewTabLinkComponent
 }
 import views.html.components.download_link_cash_account
 
@@ -130,9 +131,9 @@ object CashStatementViewModel {
       .flatMap(_.cashAccountNumber)
       .headOption match {
       case Some(cashAccountNumber) =>
-        h2Component(
-          msg = messages("cf.cash-statement-requested-account-heading", cashAccountNumber),
-          classes = "govuk-caption-xl"
+        descriptionListCaption(
+          dtMsg = messages("cf.cash-statement-requested-account-heading", cashAccountNumber),
+          ddMsg = cashAccountNumber
         )
 
       case None => Html(emptyString)
